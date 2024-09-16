a1d6250a6286c898c1415265a004548591ffb42e
Wes Hofmann <wes@appreciate.it>
2024-09-16 16:12:28 -0500

    rename find-businesses.py to find_businesses.py

--- find-restaurants.sh
+++ find-restaurants.sh
@@ -13,24 +13,24 @@ function divider() {
 
 }
 
-./find-businesses.py --search-center 'Norman, OK' -n 50 --bearing 0 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Norman, OK' -n 50 --bearing 0 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Midwest City, OK' -n 200 --bearing 270 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Midwest City, OK' -n 200 --bearing 270 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'The Village, OK' -n 500 --bearing 165 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'The Village, OK' -n 500 --bearing 165 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Quail Springs Mall, OK' -n 150 --bearing 165 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Quail Springs Mall, OK' -n 150 --bearing 165 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Scissortail Park, OK' -n 150 --bearing 315 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Scissortail Park, OK' -n 150 --bearing 315 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Moore, OK' -n 175 --bearing 0 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Moore, OK' -n 175 --bearing 0 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Edmond, OK' -n 150 --bearing 225 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Edmond, OK' -n 150 --bearing 225 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Yukon, OK' -n 150 --bearing 90 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Yukon, OK' -n 150 --bearing 90 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Bethany, OK' -n 100 --bearing 45 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Bethany, OK' -n 100 --bearing 45 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
-./find-businesses.py --search-center 'Oklahoma City, OK' -n 100 --bearing 170 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
+./find_businesses.py --search-center 'Oklahoma City, OK' -n 100 --bearing 170 --search-radius $RADIUS -d $DISTANCE -t $BUSINESS_TYPE
 divider
 ./make_csv.py -t $BUSINESS_TYPE
